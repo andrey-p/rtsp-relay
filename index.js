f6dd8d7eddf1f25761a249e369b1b4fca986619c
Andrey Pissantchev <andrey.pissantchev@protonmail.com>
2021-05-19 19:14:42 +0100

    split out inboundstreamwrapper from index

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const { spawn } = require('child_process');
 const ews = require('express-ws');
 const ps = require('ps-node');
 const { version } = require('./package.json');
+const InboundStreamWrapper = require('./InboundStreamWrapper.js');
 
 /**
  * @typedef {{
@@ -16,74 +17,6 @@ const { version } = require('./package.json');
  * @typedef {import("ws")} WebSocket
  * @typedef {import("child_process").ChildProcessWithoutNullStreams} Stream
  */
-
-class InboundStreamWrapper {
-  /** @param {Options} props */
-  start({ url, additionalFlags = [] }) {
-    if (this.verbose) console.log('[rtsp-relay] Creating brand new stream');
-
-    this.stream = spawn(
-      ffmpegPath,
-      [
-        '-i',
-        url,
-        '-f', // force format
-        'mpegts',
-        '-codec:v', // specify video codec (MPEG1 required for jsmpeg)
-        'mpeg1video',
-        '-r',
-        '30', // 30 fps. any lower and the client can't decode it
-        ...additionalFlags,
-        '-',
-      ],
-      { detached: false },
-    );
-    this.stream.stderr.on('data', () => {});
-    this.stream.stderr.on('error', (e) => console.log('err:error', e));
-    this.stream.stdout.on('error', (e) => console.log('out:error', e));
-    this.stream.on('error', (err) => {
-      if (this.verbose) {
-        console.warn(`[rtsp-relay] Internal Error: ${err.message}`);
-      }
-    });
-
-    this.stream.on('exit', (_code, signal) => {
-      if (signal !== 'SIGTERM') {
-        if (this.verbose) {
-          console.warn(
-            '[rtsp-relay] Stream died - will recreate when the next client connects',
-          );
-        }
-        this.stream = null;
-      }
-    });
-  }
-
-  /** @param {Options} options */
-  get(options) {
-    this.verbose = options.verbose;
-    if (!this.stream) this.start(options);
-    return /** @type {Stream} */ (this.stream);
-  }
-
-  /** @param {number} clientsLeft */
-  kill(clientsLeft) {
-    if (!this.stream) return; // the stream is currently dead
-    if (!clientsLeft) {
-      if (this.verbose) {
-        console.log('[rtsp-relay] no clients left; destroying stream');
-      }
-      this.stream.kill('SIGTERM');
-      this.stream = null;
-      // next time it is requested it will be recreated
-    }
-    if (this.verbose) {
-      console.log(
-        '[rtsp-relay] there are still some clients so not destroying stream',
-      );
-    }
-  }
-}
 
 /** @type {ReturnType<ews>} */
 let wsInstance;
